b73486a2c0bb5784b7285363ab8760a8f8464e90
asmagomedova <m.h.axm@mail.ru>
2023-12-04 13:46:13 +0300

    v1

--- tests/test_post_users_negative.py
+++ tests/test_post_users_negative.py
@@ -1,35 +1,26 @@
-from hamcrest import assert_that, equal_to
+from hamcrest import assert_that, equal_to, is_not
 from tests.data.data import NAME_AND_JOB_NEG
 
 def test_create_with_empty_string(reqres_api):
     status_code, response = reqres_api.create_user(NAME_AND_JOB_NEG[0], NAME_AND_JOB_NEG[0])
-    assert_that(status_code, equal_to(201))
-    assert_that(response['name'], equal_to(NAME_AND_JOB_NEG[0]))
-    assert_that(response.get('job'), equal_to(NAME_AND_JOB_NEG[0]))
+    assert_that(status_code, is_not(201))
 
 def test_create_with_symbol(reqres_api):
     status_code, response = reqres_api.create_user(NAME_AND_JOB_NEG[1], NAME_AND_JOB_NEG[1])
-    assert_that(status_code, equal_to(201))
-    assert_that(response['name'], equal_to(NAME_AND_JOB_NEG[1]))
-    assert_that(response.get('job'), equal_to(NAME_AND_JOB_NEG[1]))
+    assert_that(status_code, is_not(201))
 
 def test_create_with_whitespace(reqres_api):
     status_code, response = reqres_api.create_user(NAME_AND_JOB_NEG[2], NAME_AND_JOB_NEG[2])
-    assert_that(status_code, equal_to(201))
-    assert_that(response['name'], equal_to(NAME_AND_JOB_NEG[2]))
-    assert_that(response.get('job'), equal_to(NAME_AND_JOB_NEG[2]))
+    assert_that(status_code, is_not(201))
 
 def test_create_with_fractional(reqres_api):
     status_code, response = reqres_api.create_user(NAME_AND_JOB_NEG[3], NAME_AND_JOB_NEG[3])
-    assert_that(status_code, equal_to(201))
-    assert_that(float(response['name']), equal_to(NAME_AND_JOB_NEG[3]))
-    assert_that(float(response.get('job')), equal_to(NAME_AND_JOB_NEG[3]))
+    assert_that(status_code, is_not(201))
 
 def test_create_with_integer(reqres_api):
     status_code, response = reqres_api.create_user(NAME_AND_JOB_NEG[4], NAME_AND_JOB_NEG[4])
-    assert_that(status_code, equal_to(201))
-    assert_that(int(response['name']), equal_to(NAME_AND_JOB_NEG[4]))
-    assert_that(int(response.get('job')), equal_to(NAME_AND_JOB_NEG[4]))
+    assert_that(status_code, is_not(201))
+
 
 def test_create_string__with_characters_and_spaces(reqres_api):
     status_code, response = reqres_api.create_user(NAME_AND_JOB_NEG[5], NAME_AND_JOB_NEG[5])
@@ -39,6 +30,5 @@ def test_create_string__with_characters_and_spaces(reqres_api):
 
 def test_create_with_XSS(reqres_api):
     status_code, response = reqres_api.create_user(NAME_AND_JOB_NEG[6], NAME_AND_JOB_NEG[6])
-    assert_that(status_code, equal_to(201))
-    assert_that(response['name'], equal_to(NAME_AND_JOB_NEG[6]))
-    assert_that(response.get('job'), equal_to(NAME_AND_JOB_NEG[6]))
+    print(response)
+    assert_that(status_code, is_not(201))
