31340b93927385653ca974527884bb9d86068628
asmagomedova <m.h.axm@mail.ru>
2023-12-20 12:24:12 +0300

    v1

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
 import pytest
 
-from config import HOST
 from framework.reqres_api import ReqresAPI
 
 @pytest.fixture(scope="session")
 def reqres_api():
+   HOST = 'https://reqres.in'
    return ReqresAPI(HOST)
